a163f8522540dadcb93f6c7ade37996b240af3d9
budlinville <budlinville@gmail.com>
2023-05-30 15:27:20 -0500

    Small template changes

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -6,7 +6,7 @@ const Home = ({ }: HomeProps) => {
     return ( 
         <div>
             <h1>Home</h1>
-            { [...new Array(30)].map( () => `
+            { [...new Array(100)].map( () => `
                 Cras mattis consectetur purus sit amet fermentum.
                 Cras justo odio, dapibus ac facilisis in, egestas eget quam.
                 Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
